2d172367e6d38fe6b00ce08be26f8ecd3b05c27b
Kenny Gamer <KGamer@gamerpackaging.com>
2017-08-28 16:39:26 -0400

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Charlie Gamer on 8/28/17.
+//  Copyright © 2017 Charlie Gamer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
